a9e6574c37bc49589df566b02e618978a52e7159
rosexalba <rosemaiellalv@gmail.com>
2025-08-22 11:38:06 -0700

    if else

---
+++ Control Flow in JavaScript/conditional-statement.js
@@ -0,0 +1,35 @@
+// if statement
+
+let temperature = 30;
+
+if (temperature > 25) {
+    console.log("It is a hot day! Stay hydrated.")
+}
+
+
+// if.. else if.. else
+
+const marks = 80;
+
+if(marks >= 90) {
+    console.log('Grade A');
+} else if  (marks >= 80) {
+    console.log("Grade: B");
+} else if (marks >= 70) {
+    console.log("Grade: C");
+} else if(marks >= 60) {
+    console.log("Grade: D")
+} else {
+    console.log("Grade:F")
+}
+
+// if..else 
+
+   const isWeekend = false;
+
+   if (isWeekend) {
+    console.log("Enjoy your weekend")
+   } else {
+    console.log("Time to Work!")
+   }
+
--- Control Flow in JavaScript/notes.md
+++ Control Flow in JavaScript/notes.md
@@ -6,3 +6,13 @@ What will you learn in this lesson?
 - Mini Project: To-Do List with Loop Operations
 
 # Conditionals: if Statements 
+Conditional statements control the flow of a program by executing different blocks of code based on conditions
+
+1. if statement 
+- executes a block of code if the condition is true. 
+
+let temperature = 30;
+
+if (temperature > 25) {
+    console.log("It is a hot day! Stay hydrated.")
+}
