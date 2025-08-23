862c7d725b1dcb9e841c7ff0a5f0552a827a594c
rosexalba <rosemaiellalv@gmail.com>
2025-08-23 11:28:03 -0700

    Array Methods

--- Arrays and Objects/arrays.js
+++ Arrays and Objects/arrays.js
@@ -1,56 +1,56 @@
-// const fruits = ["Apple", "banana", "Cherry"];
-// console.log(fruits); // Output: ["Apple", "banana", "Cherry"];
+const fruits = ["Apple", "banana", "Cherry"];
+console.log(fruits); // Output: ["Apple", "banana", "Cherry"];
 
-// // Declaring & Manipulating Arrays 
-// // ACCESSING THE ARRAY ELEMENT: Arrays are 0 indexed - zero is the first item
-// // so if you console.log fruits at the zero index youll see that it says apple
+// Declaring & Manipulating Arrays 
+// ACCESSING THE ARRAY ELEMENT: Arrays are 0 indexed - zero is the first item
+// so if you console.log fruits at the zero index youll see that it says apple
 
-//       console.log(fruits[0]);
+      console.log(fruits[0]);
 
-// // if you console.log at the 2 you will see Cherry
+// if you console.log at the 2 you will see Cherry
 
-//       console.log(fruits[2]);
+      console.log(fruits[2]);
 
 
-// // CHANGING ARRAY ELEMENTS: modify array values directly 
+// CHANGING ARRAY ELEMENTS: modify array values directly 
 
-//       console.log(fruits[1]); // output: banana 
+      console.log(fruits[1]); // output: banana 
 
-// // you can change it by saying fruits and then assign a new value there.
+// you can change it by saying fruits and then assign a new value there.
       
-//       fruits[1] = "Blueberry"
-//       console.log(fruits);
+      fruits[1] = "Blueberry"
+      console.log(fruits);
 
-//  // FINDING THE LENGTH OF AN ARRAY:
+ // FINDING THE LENGTH OF AN ARRAY:
 
-//      console.log(fruits.length); // Output:3
+     console.log(fruits.length); // Output:3
 
 
 // ARRAY METHODS:
-//       // push - adds to end
-//    fruits.push("Mango");
-//    console.log(fruits); 
+      // push - adds to end
+   fruits.push("Mango");
+   console.log(fruits); 
 
-//      // pop - removes from end
-//    fruits.pop();
-//    console.log(fruits);
+     // pop - removes from end
+   fruits.pop();
+   console.log(fruits);
 
-//      // shift - removes from the beginning  
-//    fruits.shift();
-//    console.log(fruits);
+     // shift - removes from the beginning  
+   fruits.shift();
+   console.log(fruits);
 
-//      // Unshift - adds to the beginning 
-//    fruits.unshift("Grapes");
-//    console.log(fruits);
+     // Unshift - adds to the beginning 
+   fruits.unshift("Grapes");
+   console.log(fruits);
 
-//      // Splice - add / removes elements fruits.splice(index one, adding one, "new element")
+     // Splice - add / removes elements fruits.splice(index one, adding one, "new element")
 
-//    fruits.splice(1, 1, "peach");
-//    console.log(fruits);
+   fruits.splice(1, 1, "peach");
+   console.log(fruits);
    
-//      // Splice- remove / run: fruits.splice(from index one,remove 2 elements)
-//    fruits.splice(1,2);
-//    console.log(fruits);
+     // Splice- remove / run: fruits.splice(from index one,remove 2 elements)
+   fruits.splice(1,2);
+   console.log(fruits);
 
      // Map: 
      const numbers = [1,2,3,4,5];
